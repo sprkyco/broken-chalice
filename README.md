e13c894ddf517e88014e95761f760af66b93703c
sprkyco@gmail.com <sprkyco@gmail.com>
2018-03-08 18:08:40 -0800

    scale16x up

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Don't recommend using this, but if you really, really want to....
 
-## Run me
+## Run:
 
 1. [Setup Chalice](https://github.com/aws/chalice)
 2. `git clone` me
-3. `cd broken-chalice && chalice deploy`
-4. DO NOT LEAVE THIS RUNNING! `chalice delete`
+3. `chalice new-project app && cp app.py app/ && cd app/ && chalice deploy`
+4. `chalice delete` when all done!
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from chalice import Chalice
 from os import popen
+from chalice import Chalice, Response
+
 
 app = Chalice(app_name='broken-chalice')
+app.debug = True
 
 def rp(command):
     return popen(command).read()
@@ -9,3 +11,19 @@ def rp(command):
 @app.route('/{address}')
 def nslookup(address):
     return rp("nslookup "+address)
+
+# code injection
+@app.route('/evaluate', methods = ['POST', 'GET'], content_types=['application/json'])
+def evaluate():
+    expression = None
+    if app.current_request.method == 'POST':
+        expression = app.current_request.json_body['expression']
+    return "Result:\n" + (str(eval(expression)).replace('\n', '\n')  if expression else "No expression provided")
+    
+@app.route('/trial-balloon-art', methods=['POST'])
+def getTrialBalloonArt():
+    request = app.current_request
+    if request.method == 'POST':
+        version = request.json_body['version']
+        print("version equals: "+version)
+        return rp("curl https://dl.signalsciences.net/trial-balloon/{}/art".format(version))
