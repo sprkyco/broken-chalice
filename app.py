1b399e0bf8c0897e1e018fbfda987fde210b91bc
sprkyco@gmail.com <sprkyco@gmail.com>
2018-03-10 15:57:32 -0800

    scale ready

--- app.py
+++ app.py
@@ -8,22 +8,20 @@ app.debug = True
 def rp(command):
     return popen(command).read()
 
-@app.route('/{address}')
-def nslookup(address):
-    return rp("nslookup "+address)
-
-# code injection
+# Code Injection
 @app.route('/evaluate', methods = ['POST', 'GET'], content_types=['application/json'])
 def evaluate():
     expression = None
     if app.current_request.method == 'POST':
         expression = app.current_request.json_body['expression']
     return "Result:\n" + (str(eval(expression)).replace('\n', '\n')  if expression else "No expression provided")
-    
+
+# Command Execution
 @app.route('/trial-balloon-art', methods=['POST'])
 def getTrialBalloonArt():
-    request = app.current_request
     if request.method == 'POST':
-        version = request.json_body['version']
+        version = app.current_request.json_body['version']
         print("version equals: "+version)
-        return rp("curl https://dl.signalsciences.net/trial-balloon/{}/art".format(version))
+        sigsciBalloon = "https://dl.signalsciences.net/trial-balloon/{}/art".format(version)
+        out = rp("curl {}".format(sigsciBalloon))
+        return out
